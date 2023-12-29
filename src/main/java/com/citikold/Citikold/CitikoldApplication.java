1764b08122fa647feaf7d938bd7ce71b4460e2ed
Angelo <chise_elunico@hotmail.com>
2023-12-28 19:31:21 -0500

    First commit

---
+++ src/main/java/com/citikold/Citikold/CitikoldApplication.java
@@ -0,0 +1,15 @@
+package com.citikold.Citikold;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitikoldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitikoldApplication.class, args);
+
+		System.out.println("HELLO CITIKOLD");
+	}
+
+}
---
+++ src/test/java/com/citikold/Citikold/CitikoldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.citikold.Citikold;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitikoldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
